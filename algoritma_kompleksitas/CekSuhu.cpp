34242e5a5136174462291a639e258218afa6cfe9
Rizki Wahyu Pratama <69743247+putunebandi@users.noreply.github.com>
2025-10-15 10:41:54 +0700

    Update CekSuhu.cpp

--- algoritma_kompleksitas/CekSuhu.cpp
+++ algoritma_kompleksitas/CekSuhu.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 
 int main() {
-    int suhu = 36; [span_0](start_span)
-
+    int suhu = 36;
     std::cout << "Suhu saat ini: " << suhu << " derajat." << std::endl;
-
-    [span_1](start_span)
     if (suhu < 38) {
         std::cout << "Status: Suhu Normal" << std::endl;
     }
