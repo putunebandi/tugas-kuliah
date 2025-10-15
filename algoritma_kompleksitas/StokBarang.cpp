ce742d3e3e7e659c8479cf8e420644f1968918f3
Rizki Wahyu Pratama <69743247+putunebandi@users.noreply.github.com>
2025-10-15 10:44:26 +0700

    Update StokBarang.cpp

--- algoritma_kompleksitas/StokBarang.cpp
+++ algoritma_kompleksitas/StokBarang.cpp
@@ -2,13 +2,10 @@
 #include <string>
 
 int main() {
-    [span_6](start_span)
     char kodeBarang = 'A';
     int stok = 7;
     std::string namaBarang;
     std::string statusStok;
-
-    [span_7](start_span)
     switch (kodeBarang) {
         case 'A':
             namaBarang = "Monitor";
@@ -23,8 +20,6 @@ int main() {
             namaBarang = "Barang tidak dikenal";
             break;
     }
-
-    [span_8](start_span)
     if (stok > 10) {
         statusStok = "Stok Aman";
     } else if (stok >= 5 && stok <= 10) {
@@ -34,10 +29,8 @@ int main() {
     } else {
         statusStok = "Stok Habis";
     }
-
     std::cout << "Nama Barang: " << namaBarang << std::endl;
     std::cout << "Jumlah Stok: " << stok << std::endl;
     std::cout << "Status: " << statusStok << std::endl;
-
     return 0;
 }
